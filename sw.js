f37eb65bc46cba91f7fd4c86614301f2b2d23548
william0612 <c14200031@john.petra.ac.id>
2023-03-31 08:25:00 +0700

    y

--- sw.js
+++ sw.js
@@ -42,3 +42,26 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('first-app')
+        .then(function(cache) {
+          cache.addAll([
+            '/',
+            '/index.html'
+          ])
+        })
+    );
+    return self.clients.claim();
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(res) {
+          return res;
+        })
+    );
+  });
+  
+  
